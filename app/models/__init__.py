d33be07d0220df5a52db94875e7f75f2838460ca
Alexandra <aleksandra.alexand2017@yandex.ru>
2022-07-02 14:15:41 +0300

    second

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-from .charity_project import CharityProject  # noqa
-from .donation import Donation  # noqa
-from .user import User  # noqa
+from models.charity_project import CharityProject  # noqa
+from models.donation import Donation  # noqa
+from models.user import User  # noqa
